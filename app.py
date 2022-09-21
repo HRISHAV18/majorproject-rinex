0aa7505ff5bdfc98f783c7cc980ba5158fdaa4d9
HRISHAV18 <97503756+HRISHAV18@users.noreply.github.com>
2022-09-21 10:36:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import joblib
+
+#load the joblib model
+model = joblib.load('gas-diesel')
+
+st.title('GAS-DIESEL CLASSIFIER')  #creates a title in web app
+ip = st.text_input('Enter car name :')  #creates a text box in web app
+
+op = model.predict([ip])
+if st.button('Predict'):
+  st.title(op[0])  # st.button will create a button with name Predict
+
